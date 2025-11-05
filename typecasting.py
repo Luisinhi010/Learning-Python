da8553c48fcb14696f0ab4abbab2fa0776e63ac9
Luisinho01 <Luiscpc3@gmail.com>
2025-11-05 17:33:43 -0300

    exemplos básicos de entrada e impressão e variáveis

---
+++ typecasting.py
@@ -0,0 +1,5 @@
+name = "Luis Claudio"
+age = 19
+gpa = 3.0
+isStudent = True
+
---
+++ variable.py
@@ -0,0 +1,20 @@
+
+#Strings
+firstName = "Luis"
+food = "chocolate"
+email = "Luis@fake.com"
+
+#Integers
+age = 19
+quantity = 3
+numOfStudents = 30
+
+#Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+#Boolean
+isStudent = False
+forSale = False
+isOnline = False
